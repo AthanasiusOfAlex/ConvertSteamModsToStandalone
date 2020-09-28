4aab67720fe0838b489d89022ed25f5bca642b34
Louis Melahn, L.C <lmelahn@legionaries.org>
2020-09-28 11:08:30 +0200

    updated

--- ConvertSteamModsToStandalone/ConvertSteamModsToStandalone.py
+++ ConvertSteamModsToStandalone/ConvertSteamModsToStandalone.py
@@ -6,7 +6,7 @@ steamCSDir = Path("C:\\Program Files (x86)\\Steam\\steamapps\\workshop\\content\
 workingDir = Path("C:\\Users\\louis\\Desktop\\cstemp")
 
 # Set up the working directories and copy mods and assets
-if workingDir.exists:
+if workingDir.exists():
 	try:
 		shutil.rmtree(workingDir)
 		pass
@@ -39,13 +39,25 @@ for dirName in workingDir.iterdir():
 			dir.rename(workingDir / "ParallelRoadTool")
 			continue
 
+		# Place CSURLoader.dll in its own folder with its own name, and keep the assets in a numbered folder.
 		if dir.name == "1959183067":
 			fileToMove = dir / "CSURLoader.dll"
-			if fileToMove.exists():
+			if fileToMove.exists:
 				newAssetDir = workingDir / fileToMove.stem
 				os.mkdir(newAssetDir)
 				fileToMove.rename(newAssetDir / fileToMove.name)
 
+		# Removes the "Sources" directory in the University City mod, which causes problems.
+		if dir.name == "1747800340":
+			dirToDelete = dir / "Source"
+			if dirToDelete.exists():
+				try:
+					shutil.rmtree(dirToDelete)
+					pass
+				except:
+					pass
+		
+		# Change directories with mods to the name of the DLL
 		for fileName in dir.iterdir():
 			file = Path(fileName)
 			
